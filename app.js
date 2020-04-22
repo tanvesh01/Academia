f426cd8515969916fc34dfe4f2f3f7106cbde897
tanvesh <sarvetanvesh01@gmail.com>
2020-04-23 00:54:26 +0530

    Connected mongoDB

--- app.js
+++ app.js
@@ -8,15 +8,24 @@ var express = require("express"),
     User = require("./modals/User"),
     asyncLoop = require("node-async-loop");
 
-mongoose.connect(
-    "mongodb://localhost/acadmia",
-    {
-        useNewUrlParser: true,
-    },
-    function () {
-        console.log("connected");
-    }
-);
+mongoose
+    .connect(
+        "mongodb+srv://tanvesh01:dedsec01@academia-wysgv.mongodb.net/test?retryWrites=true&w=majority",
+        {
+            useNewUrlParser: true,
+            useCreateIndex: true,
+        }
+    )
+    .then(() => {
+        console.log("connected to DB");
+    })
+    .catch((err) => {
+        console.log("ERROR:", err.message);
+    });
+// function () {
+//     console.log("connected");
+// }
+
 app.use(
     bodyParser.urlencoded({
         extended: true,
@@ -40,6 +49,23 @@ passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
+// Course.create(
+//     {
+//         name: "CS50 lectures by Stanford University",
+//         link:
+//             "https://www.youtube.com/playlist?list=PLhQjrBD2T381L3iZyDTxRwOBuUt6m1FnW",
+//         image:
+//             "https://prod-discovery.edx-cdn.org/media/course/image/0c675c29-54ee-42ff-ac95-01f53450bc8b-aaffd76ac491.small.jpg",
+//         type: "Video lectures",
+//     },
+//     function (err, course) {
+//         if (err) {
+//             console.lof(err);
+//         } else {
+//             console.log(course);
+//         }
+//     }
+// );
 //Auth routes ===============================================
 app.get("/register", (req, res) => {
     res.render("./authViews/register");
@@ -168,13 +194,13 @@ app.get("/all/:id", (req, res) => {
     });
 });
 
-// app.listen(3000, () => {
-//     console.log("Academia has started!!");
-// });
-
-app.listen(process.env.PORT, process.env.IP, function () {
-    console.log("This shit started!!!!!!");
+app.listen(3000, () => {
+    console.log("Academia has started!!");
 });
+
+// app.listen(process.env.PORT, process.env.IP, function () {
+//     console.log("This shit started!!!!!!");
+// });
 
 // Course.create({
 //     name: "CS50 lectures by Stanford University",
